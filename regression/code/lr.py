b96929be650676cdc299053597f12dbac8ba711a
Xinfan Meng <mxf3306@gmail.com>
2012-02-25 22:40:51 +0800

    lasso and ridge regression

--- regression/code/lr.py
+++ regression/code/lr.py
@@ -2,9 +2,10 @@
 Conduct linear regression y ~ bx
 """
 from sklearn.datasets import load_svmlight_file
-from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.linear_model import LinearRegression, Lasso, ElasticNet, Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.cross_validation import KFold
+import pylab as pl
 
 def regression(data_file):
     """
@@ -15,14 +16,22 @@ def regression(data_file):
     data_file: a libsvm data file
     """
     X, y = load_svmlight_file(data_file)
-    X = X.todense()
-    kf = KFold(len(y), 4, indices=True)
+    kf = KFold(len(y), 2, indices=True)
     #lr = LinearRegression()
-    lr = Lasso()
+    #lr = Ridge(alpha=10)
+    X = X.todense()
+    lr = Lasso(alpha=0.003)
+    #lr = ElasticNet()
     for train, test in kf:
         X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
         lr.fit(X_train, y_train)
         y_predict = lr.predict(X_test)
+        #pl.hist(y_predict)
+        #pl.hist(lr.coef_)
+        pl.scatter(y_predict, y_test)
+        pl.xlabel("Predicted polarity")
+        pl.ylabel("True polarity")
+        pl.show()
         print "mse", mean_squared_error(y_test, y_predict)
         print "r^2", lr.score(X_test, y_test)
 
