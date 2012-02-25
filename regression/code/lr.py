3eb62af76ac024cdb71685a9137c4f039cf8ad19
Xinfan Meng <mxf3306@gmail.com>
2012-02-25 18:38:13 +0800

    lasso regerssion

--- regression/code/lr.py
+++ regression/code/lr.py
@@ -2,7 +2,7 @@
 Conduct linear regression y ~ bx
 """
 from sklearn.datasets import load_svmlight_file
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.metrics import mean_squared_error
 from sklearn.cross_validation import KFold
 
@@ -15,15 +15,16 @@ def regression(data_file):
     data_file: a libsvm data file
     """
     X, y = load_svmlight_file(data_file)
-    kf = KFold(len(y), 2, indices=False)
-    print X.shape, y.shape
+    X = X.todense()
+    kf = KFold(len(y), 4, indices=True)
+    #lr = LinearRegression()
+    lr = Lasso()
     for train, test in kf:
         X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
-        print X_train.shape, y_train.shape
-        lr = LinearRegression()
         lr.fit(X_train, y_train)
         y_predict = lr.predict(X_test)
-        print mean_squared_error(y_test, y_predict)
+        print "mse", mean_squared_error(y_test, y_predict)
+        print "r^2", lr.score(X_test, y_test)
 
 if __name__ == "__main__":
     data_file = "../1average_label/1-2yx.txt"
